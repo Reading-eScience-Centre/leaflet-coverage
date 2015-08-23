9089f0ce0c62d70bdc3eb282be92cd2ab361d135
Maik Riechert <maik.riechert@arcor.de>
2015-08-23 20:14:42 +0100

    support asc/desc array ordering in indicesOfNearest()

--- src/renderers/utils.js
+++ src/renderers/utils.js
@@ -1,6 +1,6 @@
 /***
  * Return the indices of the two neighbors in the a array closest to x.
- * The array must be sorted, either ascending or descending.
+ * The array must be sorted (strictly monotone), either ascending or descending.
  * 
  * If x exists in the array, both neighbors point to x.
  * If x is lower (greated if descending) than the first value, both neighbors point to 0.
@@ -9,15 +9,30 @@
  * Adapted from https://stackoverflow.com/a/4431347
  */
 export function indicesOfNearest (a, x) {
-  // TODO handle descending arrays
+  if (a.length === 0) {
+    throw new Error('Array must have at least one element')
+  }
   var lo = -1
   var hi = a.length
-  while (hi - lo > 1) {
-    let mid = Math.round((lo + hi) / 2)
-    if (a[mid] <= x) {
-      lo = mid
-    } else {
-      hi = mid
+  const ascending = a.length === 1 || a[0] < a[1]
+  // we have two separate code paths to help the runtime optimize the loop
+  if (ascending) {
+    while (hi - lo > 1) {
+      let mid = Math.round((lo + hi) / 2)
+      if (a[mid] <= x) {
+        lo = mid
+      } else {
+        hi = mid
+      }
+    }
+  } else {
+    while (hi - lo > 1) {
+      let mid = Math.round((lo + hi) / 2)
+      if (a[mid] >= x) { // here's the difference
+        lo = mid
+      } else {
+        hi = mid
+      }
     }
   }
   if (a[lo] === x) hi = lo
@@ -26,6 +41,10 @@ export function indicesOfNearest (a, x) {
   return [lo, hi]
 }
 
+/**
+ * Return the index in a of the value closest to x.
+ * The array a must be sorted, either ascending or descending.
+ */
 export function indexOfNearest (a, x) {
   var i = indicesOfNearest(a, x)
   var lo = i[0]
