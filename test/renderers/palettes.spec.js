eac0d2fb29d536a6a356333938acb541883088e8
Maik Riechert <maik.riechert@arcor.de>
2015-09-01 10:51:02 +0100

    one more palette test

--- test/renderers/palettes.spec.js
+++ test/renderers/palettes.spec.js
@@ -2,9 +2,9 @@ import assert from 'assert'
 
 import * as palettes from 'leaflet-coverage/renderers/palettes.js'
 
-describe("renderers/palettes methods", () => {
-  describe("#directPalette", () => {
-    it("returns an identical palette", () => {
+describe('renderers/palettes', () => {
+  describe('#directPalette', () => {
+    it('returns an identical palette', () => {
       let r = [100,23,10]
       let g = [13,50,123]
       let b = [10,99,255]
@@ -18,6 +18,18 @@ describe("renderers/palettes methods", () => {
         assert.strictEqual(palette.green[i], g[i])
         assert.strictEqual(palette.blue[i], b[i])
       }
+    })
+  })
+  describe('#PaletteManager', () => {
+    describe('#addLinear', () => {
+      let steps = 10
+      let pm = new palettes.PaletteManager({defaultSteps: steps})
+      pm.addLinear('blues', ['#deebf7', '#3182bd'])
+      let blues = pm.get('blues')
+      assert.equal(blues.steps, steps)
+      assert.equal(blues.red.length, steps)
+      assert.equal(blues.green.length, steps)
+      assert.equal(blues.blue.length, steps)
     })
   })
 })
