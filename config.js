3fc9d326bad3be93065d24049108514467d87feb
Maik Riechert <maik.riechert@arcor.de>
2015-09-24 17:54:59 +0100

    update deps

--- config.js
+++ config.js
@@ -14,9 +14,9 @@ System.config({
 
   map: {
     "assert": "npm:assert@1.3.0",
-    "babel": "npm:babel-core@5.8.22",
-    "babel-runtime": "npm:babel-runtime@5.8.20",
-    "core-js": "npm:core-js@1.1.1",
+    "babel": "npm:babel-core@5.8.25",
+    "babel-runtime": "npm:babel-runtime@5.8.25",
+    "core-js": "npm:core-js@1.1.4",
     "cwise-compiler": "npm:cwise-compiler@1.1.2",
     "leaflet": "github:Leaflet/Leaflet@0.7.3",
     "minified": "github:timjansen/minified.js@2015.1.1",
@@ -33,7 +33,7 @@ System.config({
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
     },
-    "npm:babel-runtime@5.8.20": {
+    "npm:babel-runtime@5.8.25": {
       "process": "github:jspm/nodelibs-process@0.1.1"
     },
     "npm:buffer@3.4.3": {
@@ -41,7 +41,7 @@ System.config({
       "ieee754": "npm:ieee754@1.1.6",
       "is-array": "npm:is-array@1.0.1"
     },
-    "npm:core-js@1.1.1": {
+    "npm:core-js@1.1.4": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
       "process": "github:jspm/nodelibs-process@0.1.1",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
