cb9110cdaf2c12c9e7f4e95d906e03b441925982
Maik Riechert <maik.riechert@arcor.de>
2015-10-08 11:54:19 +0100

    handle shallow cloning of maps

--- leaflet-coverage/util/transform.js
+++ leaflet-coverage/util/transform.js
@@ -14,18 +14,25 @@ export function withParameters (cov, params) {
  */
 export function withCategories (cov, key, categories) {
   let newparams = shallowcopy(cov.parameters)
-  newparams[key].categories = categories
+  newparams.get(key).categories = categories
   let newcov = withParameters(cov, newparams)
   return newcov
 }
 
 /**
  * Shallow clone a given object.
+ * 
+ * Note: This does *not* handle all kinds of objects!
  */
 function shallowcopy (obj) {
-  let copy = Object.create(Object.getPrototypeOf(obj))
-  for (let prop in obj) {
-    copy[prop] = obj[prop]
+  let copy
+  if (obj instanceof Map) {
+    copy = new Map(obj)
+  } else {
+    copy = Object.create(Object.getPrototypeOf(obj))
+    for (let prop in obj) {
+      copy[prop] = obj[prop]
+    } 
   }
   return copy
 }
