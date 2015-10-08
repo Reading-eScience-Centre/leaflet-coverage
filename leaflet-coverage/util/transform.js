eacecdce9b4c1fbca7be23ff2ab3adbdc969e4e5
Maik Riechert <maik.riechert@arcor.de>
2015-10-08 11:26:36 +0100

    add some coverage transformation utilities

---
+++ leaflet-coverage/util/transform.js
@@ -0,0 +1,28 @@
+/**
+ * Returns a copy of the given Coverage object with the parameters
+ * replaced by the supplied ones.
+ */
+export function withParameters (cov, params) {
+  let newcov = shallowcopy(cov)
+  newcov.parameters = params  
+  return newcov
+}
+
+/**
+ * Returns a copy of the given Coverage object with the categories
+ * of a given parameter replaced by the supplied ones.
+ */
+export function withCategories (cov, key, categories) {
+  let newparams = shallowcopy(cov.parameters)
+  newparams[key].categories = categories
+  let newcov = withParameters(cov, newparams)
+  return newcov
+}
+
+function shallowcopy (obj) {
+  let copy = {}
+  for (let prop in obj) {
+    copy[prop] = obj[prop]
+  }
+  return copy
+}
