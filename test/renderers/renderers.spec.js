b6240a983135702873e947f9ac2cce0ca0a9ae73
Maik Riechert <maik.riechert@arcor.de>
2015-12-01 22:07:59 +0000

    fix import in tests

--- test/renderers/renderers.spec.js
+++ test/renderers/renderers.spec.js
@@ -1,5 +1,5 @@
 import assert from 'assert'
 
 import Grid from 'leaflet-coverage/renderers/Grid.js'
-import Profile from 'leaflet-coverage/renderers/Profile.js'
+import VerticalProfile from 'leaflet-coverage/renderers/VerticalProfile.js'
 import Trajectory from 'leaflet-coverage/renderers/Trajectory.js'
