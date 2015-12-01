2b8ef6162a7b389e23df9d352ccb85a81067d3b4
Maik Riechert <maik.riechert@arcor.de>
2015-12-01 22:27:47 +0000

    try to speed up range reduce()

--- leaflet-coverage/util/range.js
+++ leaflet-coverage/util/range.js
@@ -1,22 +1,34 @@
 /**
- * Apply a reduce function over the range values.
+ * Iterate over all range values and run a function for each value.
  */
-export function reduce (range, fn, start) {
-  function it (obj, shape, axisIdx, v1) {
+export function iterate (range, fn) {
+  // TODO create pre-compiled for-loop version to improve speed
+  function iterateRecurse (obj, shape, axisIdx) {
     if (shape.length === axisIdx) {
-      let v2 = range.get(obj)
-      return fn(v1, v2)
+      let val = range.get(obj)
+      fn(val, obj)
     } else {
       let [axis,size] = shape[axisIdx]
       for (let i=0; i < size; i++) {
         obj[axis] = i
-        v1 = it(obj, shape, axisIdx+1, v1)
+        iterateRecurse(obj, shape, axisIdx+1)
       }
-      return v1
     }
   }
   let shape = [...range.shape]
-  return it({}, shape, 0, start)
+  return iterateRecurse({}, shape, 0)
+}
+
+/**
+ * Apply a reduce function over the range values.
+ */
+export function reduce (range, fn, start) {
+  let v1 = start
+  let iterFn = v2 => {
+    v1 = fn(v1, v2)
+  }
+  iterate(range, iterFn)
+  return v1
 }
 
 /**
@@ -24,16 +36,13 @@ export function reduce (range, fn, start) {
  * ignoring null's.
  */
 export function minMax (range) {
-  return [
-   reduce(range, (v1,v2) => {
-     // minimum ignoring null's
-     if (v2 === null) return v1
-     else return v1 < v2 ? v1 : v2
-     }, Infinity),
-   reduce(range, (v1,v2) => {
-     // maximum ignoring null's
-     if (v2 === null) return v1
-     else return v1 > v2 ? v1 : v2
-     }, -Infinity)
-   ]
+  let min = Infinity
+  let max = -Infinity
+  let fn = val => {
+    if (val === null) return
+    if (val < min) min = val
+    if (val > max) max = val
+  }
+  iterate(range, fn)
+  return [min, max]
 }
