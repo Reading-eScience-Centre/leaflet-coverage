6c3a1b452bcf56c442b18ebcc5e3db3ec9388ae6
Maik Riechert <maik.riechert@arcor.de>
2015-12-02 20:57:03 +0000

    speed up range iteration by precompilation
    Firefox: 2x faster
    Chrome: nearly no change (2% faster, and 6x slower than Firefox)

--- leaflet-coverage/util/range.js
+++ leaflet-coverage/util/range.js
@@ -1,22 +1,17 @@
 /**
- * Iterate over all range values and run a function for each value.
+ * Return the minimum/maximum across all range values,
+ * ignoring null's.
  */
-export function iterate (range, fn) {
-  // TODO create pre-compiled for-loop version to improve speed
-  function iterateRecurse (obj, shape, axisIdx) {
-    if (shape.length === axisIdx) {
-      let val = range.get(obj)
-      fn(val, obj)
-    } else {
-      let [axis,size] = shape[axisIdx]
-      for (let i=0; i < size; i++) {
-        obj[axis] = i
-        iterateRecurse(obj, shape, axisIdx+1)
-      }
-    }
+export function minMax (range) {
+  let min = Infinity
+  let max = -Infinity
+  let fn = val => {
+    if (val === null) return
+    if (val < min) min = val
+    if (val > max) max = val
   }
-  let shape = [...range.shape]
-  return iterateRecurse({}, shape, 0)
+  iterate(range, fn)
+  return [min, max]
 }
 
 /**
@@ -32,17 +27,59 @@ export function reduce (range, fn, start) {
 }
 
 /**
- * Return the minimum/maximum across all range values,
- * ignoring null's.
+ * Iterate over all range values and run a function for each value.
+ * No particular iteration order must be assumed.
  */
-export function minMax (range) {
-  let min = Infinity
-  let max = -Infinity
-  let fn = val => {
-    if (val === null) return
-    if (val < min) min = val
-    if (val > max) max = val
+export function iterate (range, fn) {
+  // We use a precompiled function here for efficiency.
+  // See below for a slower recursive version.
+
+  // Benchmarks compared to recursive version:
+  // Chrome 46: around 1.03x faster
+  // Firefox 42: around 2x faster (and around 6x faster than Chrome 46!)
+  
+  // nest loops from smallest to biggest
+  let shape = [...range.shape]
+  shape.sort(([,size1], [,size2]) => size1 - size2)
+  
+  let begin = 'var obj = {}'
+  let end = ''
+  for (let [axis,size] of shape) {
+    begin += `
+      for (var i${axis}=0; i${axis} < ${size}; ++i${axis}) {
+        obj['${axis}'] = i${axis}
+    `
+    end += `}`
   }
-  iterate(range, fn)
-  return [min, max]
+  begin += `
+    fn(get(obj))
+  `
+  
+  let iterateLoop = new Function('get', 'fn', begin + end)
+  iterateLoop(range.get, fn)
 }
+
+/*
+ * Recursive version of iterate(). For reference only.
+ * 
+export function iterate (range, fn) {
+  let get = range.get
+  let shape = [...range.shape]
+  // iterate from smallest to biggest dimension
+  shape.sort(([,size1], [,size2]) => size1 - size2)
+  let dims = shape.length
+  
+  function iterateRecurse (obj, axisIdx) {
+    if (dims === axisIdx) {
+      fn(get(obj))
+    } else {
+      let [axis,size] = shape[axisIdx]
+      for (let i=0; i < size; i++) {
+        obj[axis] = i
+        iterateRecurse(obj, axisIdx+1)
+      }
+    }
+  }
+  iterateRecurse({}, 0)
+}
+*/
