2b18d26f07763fe5ad1bc18be0d37d8f9bbf941e
Maik Riechert <maik.riechert@arcor.de>
2016-05-16 17:15:33 +0100

    move more things to covutils

--- leaflet-coverage/util/referencing.js
+++ leaflet-coverage/util/referencing.js
@@ -1,1 +1,1 @@
-export {isEllipsoidalCRS, getHorizontalCRSReferenceObject, getProjection, reproject} from 'covutils/lib/referencing.js'
+export {isEllipsoidalCRS, getHorizontalCRSReferenceObject, getProjection, reproject} from 'covutils/lib/domain/referencing.js'
