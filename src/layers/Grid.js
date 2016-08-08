271b82d2bdc7585432d123a6e7fd87d0ae908c97
Maik Riechert <maik.riechert@arcor.de>
2016-08-08 22:22:08 +0100

    adapt to CovJSON changes ("components" -> "coordinates")
    see https://github.com/covjson/specification/issues/77

--- src/layers/Grid.js
+++ src/layers/Grid.js
@@ -72,7 +72,7 @@ export default class Grid extends PaletteMixin(CoverageMixin(L.TileLayer.Canvas)
       .then(() => {
         // used in controls/VerticalAxis.js
         let vertRef = getReferenceObject(this.domain, 'z')
-        if (vertRef && vertRef.components.length === 1) {
+        if (vertRef && vertRef.coordinates.length === 1) {
           let vertRefSys = vertRef.system
           if (vertRefSys.cs && (vertRefSys.cs.csAxes || vertRefSys.cs.axes)) {
             this.crsVerticalAxis = vertRefSys.cs.csAxes ? vertRefSys.cs.csAxes[0] : vertRefSys.cs.axes[0]
