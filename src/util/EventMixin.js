236886a65c712ab48c45b0ab0fb0993b9fc7b376
Maik Riechert <maik.riechert@arcor.de>
2016-10-19 15:12:12 +0100

    adapt EventMixin

--- src/util/EventMixin.js
+++ src/util/EventMixin.js
@@ -1,44 +1,54 @@
 import L from 'leaflet'
 
+let Events = L.Evented.prototype
+
 /**
- * Wraps Leaflet's L.Mixin.Events for use within class expressions.
+ * Wraps Leaflet's {@link L.Evented} for use within class expressions.
  * 
  * @see http://justinfagnani.com/2015/12/21/real-mixins-with-javascript-classes/
  * 
  * @param {class} base The base class.
- * @return {class} The base class with EventMixin.
- * 
- * @private
+ * @return {class} The base class with Leaflet's {@link L.Evented}.
  */
 export default function EventMixin (base) {
   return class extends base {
     on (...args) {
-      return L.Mixin.Events.on.call(this, ...args)
+      return Events.on.call(this, ...args)
     }
     off (...args) {
-      return L.Mixin.Events.off.call(this, ...args)
+      return Events.off.call(this, ...args)
     }
     once (...args) {
-      return L.Mixin.Events.once.call(this, ...args)
+      return Events.once.call(this, ...args)
     }
     fire (...args) {
-      return L.Mixin.Events.fire.call(this, ...args)
+      return Events.fire.call(this, ...args)
     }
-    hasEventListeners (...args) {
-      return L.Mixin.Events.hasEventListeners.call(this, ...args)
+    listens (...args) {
+      return Events.listens.call(this, ...args)
     }
     // aliases
     addEventListener (...args) {
-      return L.Mixin.Events.addEventListener.call(this, ...args)
+      return Events.addEventListener.call(this, ...args)
     }
     removeEventListener (...args) {
-      return L.Mixin.Events.removeEventListener.call(this, ...args)
+      return Events.removeEventListener.call(this, ...args)
     }
     addOneTimeEventListener (...args) {
-      return L.Mixin.Events.addOneTimeEventListener.call(this, ...args)
+      return Events.addOneTimeEventListener.call(this, ...args)
     }
     fireEvent (...args) {
-      return L.Mixin.Events.fireEvent.call(this, ...args)
+      return Events.fireEvent.call(this, ...args)
+    }
+    hasEventListeners (...args) {
+      return Events.hasEventListeners.call(this, ...args)
+    }
+    // internal
+    _on (...args) {
+      return Events._on.call(this, ...args)
+    }
+    _off (...args) {
+      return Events._on.call(this, ...args)
     }
   }
 }
