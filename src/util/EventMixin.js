3b6484948bcf25487bbdaa0c947f6b31d05df016
Maik Riechert <maik.riechert@arcor.de>
2016-10-20 11:11:05 +0100

    cleanup

--- src/util/EventMixin.js
+++ src/util/EventMixin.js
@@ -1,6 +1,6 @@
 import L from 'leaflet'
 
-let Events = L.Evented.prototype
+let Evented = L.Evented.prototype
 
 /**
  * Wraps Leaflet's {@link L.Evented} for use within class expressions.
@@ -12,49 +12,8 @@ let Events = L.Evented.prototype
  */
 export default function EventMixin (base) {
   let clazz = class extends base {}
-  for (let key in Events) {
-    clazz.prototype[key] = Events[key]
+  for (let key in Evented) {
+    clazz.prototype[key] = Evented[key]
   }
   return clazz
-  /*
-  return class extends base {
-    on (...args) {
-      return Events.on.call(this, ...args)
-    }
-    off (...args) {
-      return Events.off.call(this, ...args)
-    }
-    once (...args) {
-      return Events.once.call(this, ...args)
-    }
-    fire (...args) {
-      return Events.fire.call(this, ...args)
-    }
-    listens (...args) {
-      return Events.listens.call(this, ...args)
-    }
-    // aliases
-    addEventListener (...args) {
-      return Events.addEventListener.call(this, ...args)
-    }
-    removeEventListener (...args) {
-      return Events.removeEventListener.call(this, ...args)
-    }
-    addOneTimeEventListener (...args) {
-      return Events.addOneTimeEventListener.call(this, ...args)
-    }
-    fireEvent (...args) {
-      return Events.fireEvent.call(this, ...args)
-    }
-    hasEventListeners (...args) {
-      return Events.hasEventListeners.call(this, ...args)
-    }
-    // internal
-    _on (...args) {
-      return Events._on.call(this, ...args)
-    }
-    _off (...args) {
-      return Events._on.call(this, ...args)
-    }
-  }*/
 }
